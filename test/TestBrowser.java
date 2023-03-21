202263563f92a98cf9a951e807e16c03522d872a
Sabour Azadani <sabour.azadani@gmail.com>
2023-03-20 20:07:38 -0500

    Update TestBrowser.java
    You need to update the project again

--- test/TestBrowser.java
+++ test/TestBrowser.java
@@ -2,11 +2,12 @@ Public class TestBrowser{
 
     System.setProperty("webdriver.chrome.driver", "chromedriver");
 
-        ChromeOptions option = new ChromeOptions();
-		option.addArguments("--remote-allow-origins=*");
+    ChromeOptions option = new ChromeOptions();
+    option.addArguments("--remote-allow-origins=*");
 
-        WebDriver driver = new ChromeDriver(option);
-		driver.get("https://www.google.com/");
-
-		driver.close()
+    WebDriver driver = new ChromeDriver(option);
+    driver.get("https://www.google.com/");
+    
+    // this .close method is not required
+    driver.close()
 }
